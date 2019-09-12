2edd658a9fdba7a65d4648ab43e62a97d94305b9
Phil Arfuso <arfusop@gmail.com>
2019-09-12 16:52:01 -0400

    Added logic to remove incorrect form alert after timeout

--- src/components/auth/styled/Body.js
+++ src/components/auth/styled/Body.js
@@ -1,52 +1,52 @@
 import styled from "@emotion/styled";
 
 const Body = styled.div`
-	width: 75%;
-	display: grid;
-	grid-template-columns: 1fr;
-	justify-content: center;
-	align-items: center;
-	margin: auto;
+  width: 75%;
+  display: grid;
+  grid-template-columns: 1fr;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
 
-	.formHeader {
-		display: flex;
-		/* height: 35px; */
-		justify-content: center;
+  .formHeader {
+    display: flex;
+    /* height: 35px; */
+    justify-content: center;
     align-items: center;
     font-size: 18px;
     padding-bottom: 10px;
   }
-  
+
   .registerForm {
-    border: 1px solid #1890ff;
+    border: 2px solid #1890ff;
     padding: 20px;
     border-radius: 4px;
   }
 
-	.names {
-		display: grid;
-		grid-template-columns: 1fr 1fr;
-		grid-gap: 10px;
-	}
+  .names {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 10px;
+  }
 
-	.registerHeader {
+  .registerHeader {
     font-size: 22px;
     text-align: center;
-	}
-
-	.form {
-		display: grid;
-		width: 100%;
-		grid-gap: 10px;
-	}
-
-	.registerLoginRow {
-		height: 35px;
-		display: flex;
-		justify-content: center;
-		align-items: center;
   }
-  
+
+  .form {
+    display: grid;
+    width: 100%;
+    grid-gap: 10px;
+  }
+
+  .registerLoginRow {
+    height: 35px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
   .routeLink {
     padding-left: 3px;
   }
