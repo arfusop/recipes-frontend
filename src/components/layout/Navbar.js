729866c774b894291eaf494f608f77b78f2cfa77
Phil Arfuso <arfusop@gmail.com>
2019-09-16 11:17:54 -0400

    tried to add logout functionality. still needs work

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,22 +1,42 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import Nav from './styled/Nav';
+import Nav from "./styled/Nav";
+import { logout } from "../../store/actions/authActions";
 
 const Navbar = () => {
+  const dispatch = useDispatch();
+  const auth = useSelector(state => state.auth);
+  const { isAuthenticated, loading } = auth;
+  // console.log(auth);
+  const authLinks = (
+    <ul>
+      <li>
+        <div onClick={dispatch(logout)}>Logout</div>
+      </li>
+    </ul>
+  );
+
+  const guestLinks = (
+    <ul>
+      <li>
+        <Link to="/profile">Profile</Link>
+      </li>
+      <li>
+        <Link to="register">Sign Up</Link>
+      </li>
+      <li>
+        <Link to="login">Log In</Link>
+      </li>
+    </ul>
+  );
+
   return (
     <Nav className="navbar">
       <Link to="/">Main Course</Link>
-      <ul>
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li>
-        <li>
-          <Link to="register">Sign Up</Link>
-        </li>
-        <li>
-          <Link to="login">Log In</Link>
-        </li>
-      </ul>
+      {!loading && (
+        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+      )}
     </Nav>
   );
 };
--- src/store/reducers/authReducer/index.js
+++ src/store/reducers/authReducer/index.js
@@ -1,53 +1,55 @@
 import {
-	REGISTER_SUCCESS,
-	REGISTER_FAIL,
-	LOGIN_SUCCESS,
-	LOGIN_FAIL,
-	USER_LOADED,
-	AUTH_ERROR
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  USER_LOADED,
+  AUTH_ERROR
 } from "../../types/authTypes";
 
 const initialState = {
-	token: localStorage.getItem("token"),
-	isAuthenticated: null,
-	loading: true,
-	user: null
+  token: localStorage.getItem("token"),
+  isAuthenticated: null,
+  loading: true,
+  user: null
 };
 
 const authReducer = (state = initialState, action) => {
-	const { payload, type } = action;
+  const { payload, type } = action;
 
-	switch (type) {
-		case USER_LOADED:
-			return {
-				...state,
-				isAuthenticated: true,
-				loading: false,
-				user: payload
-			};
-		case REGISTER_SUCCESS:
-		case LOGIN_SUCCESS:
-			localStorage.setItem("token", payload.token);
-			return {
-				...state,
-				...payload,
-				isAuthenticated: true,
-				loading: false
-			};
-		case REGISTER_FAIL:
-		case LOGIN_FAIL:
-		case AUTH_ERROR:
-			localStorage.removeItem("token");
-			return {
-				...state,
-				token: null,
-				isAuthenticated: false,
-				loading: false
-			};
+  switch (type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload
+      };
+    case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
+      localStorage.setItem("token", payload.token);
+      return {
+        ...state,
+        ...payload,
+        isAuthenticated: true,
+        loading: false
+      };
+    case REGISTER_FAIL:
+    case LOGIN_FAIL:
+    case AUTH_ERROR:
+    case LOGOUT:
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false
+      };
 
-		default:
-			return state;
-	}
+    default:
+      return state;
+  }
 };
 
 export default authReducer;
