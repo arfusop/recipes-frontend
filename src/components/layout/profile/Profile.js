2cae4844bf18527d54b5b1bc2baaaa88aba3adee
Phil Arfuso <arfusop@gmail.com>
2019-09-17 16:05:44 -0400

    Fixed placeholders on profile. Added a few options to profile questions

--- src/components/layout/profile/Profile.js
+++ src/components/layout/profile/Profile.js
@@ -5,180 +5,184 @@ import { DatePicker, InputNumber, Select } from "antd";
 import ProfileBody from "../styled/ProfileBody";
 import { updateProfileReducer, initialState } from "./reducers";
 import {
-	UPDATE_DIETS,
-	UPDATE_DOB,
-	UPDATE_ALLERGIES,
-	UPDATE_SKILL,
-	UPDATE_GENDER
+  UPDATE_DIETS,
+  UPDATE_DOB,
+  UPDATE_ALLERGIES,
+  UPDATE_SKILL,
+  UPDATE_GENDER
 } from "./types";
 
 const allDiets = [
-	"Keto",
-	"Paleo",
-	"Atkins",
-	"Whole30",
-	"Vegetarian",
-	"Vegan",
-	"Gluten Free",
-	"Intermittent Fasting",
-	"Weight Watchers",
-	"mediterranean Diet"
+  "Keto",
+  "Paleo",
+  "Atkins",
+  "Whole30",
+  "Vegetarian",
+  "Vegan",
+  "Gluten Free",
+  "Intermittent Fasting",
+  "Weight Watchers",
+  "Mediterranean Diet"
 ];
 
 const allAllergies = [
-	"Milk/Dairy",
-	"Eggs",
-	"Peanuts",
-	"Tree Nuts",
-	"Shellfish",
-	"Fish",
-	"Wheat",
-	"Gluten",
-	"Soy",
-	"Fruit"
+  "Milk/Dairy",
+  "Eggs",
+  "Peanuts",
+  "Tree Nuts",
+  "Shellfish",
+  "Fish",
+  "Wheat",
+  "Gluten",
+  "Soy",
+  "Fruit"
 ];
 
 const skills = [
-	"Beginner",
-	"Intermediate",
-	"Average",
-	"Skilled",
-	"Pro",
-	"Top Chef"
+  "Beginner",
+  "Intermediate",
+  "Average",
+  "Skilled",
+  "Pro",
+  "Top Chef"
 ];
 
 const Profile = () => {
-	const isAuth = useSelector(state => state.auth.isAuthenticated);
-	const [profileData, dispatchProfile] = useReducer(
-		updateProfileReducer,
-		initialState
-	);
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
+  const [profileData, dispatchProfile] = useReducer(
+    updateProfileReducer,
+    initialState
+  );
 
-	const { allergies, cookingSkill, diets, houseHoldSize, gender } = profileData;
-	const handleDietSelections = value => {
-		dispatchProfile({ type: UPDATE_DIETS, payload: value });
-	};
+  const { allergies, cookingSkill, diets, houseHoldSize, gender } = profileData;
+  const handleDietSelections = value => {
+    dispatchProfile({ type: UPDATE_DIETS, payload: value });
+  };
 
-	const handleAllergiesSelections = value => {
-		dispatchProfile({ type: UPDATE_ALLERGIES, payload: value });
-	};
+  const handleAllergiesSelections = value => {
+    dispatchProfile({ type: UPDATE_ALLERGIES, payload: value });
+  };
 
-	const handleDOBSelection = value => {
-		dispatchProfile({ type: UPDATE_DOB, payload: value._d });
-	};
+  const handleDOBSelection = value => {
+    dispatchProfile({ type: UPDATE_DOB, payload: value._d });
+  };
 
-	const handleSkillSelection = value => {
-		dispatchProfile({ type: UPDATE_SKILL, payload: value });
-	};
+  const handleSkillSelection = value => {
+    dispatchProfile({ type: UPDATE_SKILL, payload: value });
+  };
 
-	const handleGenderSelection = value => {
-		dispatchProfile({ type: UPDATE_GENDER, payload: value });
-	};
+  const handleGenderSelection = value => {
+    dispatchProfile({ type: UPDATE_GENDER, payload: value });
+  };
 
-	const filteredDiets = allDiets.filter(o => !diets.includes(o));
-	const filteredAllergies = allAllergies.filter(a => !allergies.includes(a));
+  const filteredDiets = allDiets.filter(o => !diets.includes(o));
+  const filteredAllergies = allAllergies.filter(a => !allergies.includes(a));
 
-	if (!isAuth) {
-		return <Redirect to="/" />;
-	}
+  if (!isAuth) {
+    return <Redirect to="/" />;
+  }
 
-	return (
-		<ProfileBody>
-			<div className="profileTitle">Title & intro</div>
-			<div className="profileBody">
-				<div>login info/profile pic</div>
-				<div className="profileOptionsContainer">
-					<div className="profileOptionsTitle">Profile</div>
-					<div className="profileOptionsBody">
-						<div>
-							<span>DOB</span>
-							<DatePicker
-								// value={dob}
-								onChange={handleDOBSelection}
-								size="medium"
-							/>
-						</div>
-						<div>
-							<span>Diets</span>
-							<Select
-								mode="multiple"
-								placeholder="Diets"
-								value={diets}
-								onChange={handleDietSelections}
-								style={{ width: "100%" }}
-							>
-								{filteredDiets.map((item, index) => {
-									return (
-										<Select.Option key={index} value={item}>
-											{item}
-										</Select.Option>
-									);
-								})}
-							</Select>
-						</div>
-						<div>
-							<span>Allergies</span>
-							<Select
-								mode="multiple"
-								placeholder="Allergies"
-								value={allergies}
-								onChange={handleAllergiesSelections}
-								style={{ width: "100%" }}
-							>
-								{filteredAllergies.map((item, index) => {
-									return (
-										<Select.Option key={index} value={item}>
-											{item}
-										</Select.Option>
-									);
-								})}
-							</Select>
-						</div>
-						<div>
-							<span>Skill Level</span>
-							<Select
-								// mode="multiple"
-								placeholder="Skill Level"
-								value={cookingSkill}
-								onChange={handleSkillSelection}
-								style={{ width: "100%" }}
-							>
-								{skills.map((item, index) => {
-									return (
-										<Select.Option key={index} value={item}>
-											{item}
-										</Select.Option>
-									);
-								})}
-							</Select>
-						</div>
-						<div>
-							<span>Household Size</span>
-							<InputNumber min={1} max={10} defaultValue={1} />
-						</div>
-						<div>
-							<span>Gender</span>
-							<Select
-								// mode="multiple"
-								placeholder="Gender"
-								value={gender}
-								onChange={handleGenderSelection}
-								style={{ width: "100%" }}
-							>
-								{["Male", "Female"].map((item, index) => {
-									return (
-										<Select.Option key={index} value={item}>
-											{item}
-										</Select.Option>
-									);
-								})}
-							</Select>
-						</div>
-					</div>
-				</div>
-			</div>
-		</ProfileBody>
-	);
+  return (
+    <ProfileBody>
+      <div className="profileTitle">Title & intro</div>
+      <div className="profileBody">
+        <div>login info/profile pic</div>
+        <div className="profileOptionsContainer">
+          <div className="profileOptionsTitle">Profile</div>
+          <div className="profileOptionsBody">
+            <div>
+              <span>DOB</span>
+              <DatePicker
+                // value={dob}
+                onChange={handleDOBSelection}
+                size="medium"
+              />
+            </div>
+            <div>
+              <span>Diets</span>
+              <Select
+                showArrow
+                mode="multiple"
+                placeholder="Diets"
+                value={diets}
+                onChange={handleDietSelections}
+                style={{ width: "100%" }}
+              >
+                {filteredDiets.map((item, index) => {
+                  return (
+                    <Select.Option key={index} value={item}>
+                      {item}
+                    </Select.Option>
+                  );
+                })}
+              </Select>
+            </div>
+            <div>
+              <span>Allergies</span>
+              <Select
+                showArrow
+                mode="multiple"
+                value={allergies}
+                onChange={handleAllergiesSelections}
+                placeholder="Allergies"
+                style={{ width: "100%" }}
+              >
+                {filteredAllergies.map((item, index) => {
+                  return (
+                    <Select.Option key={index} value={item}>
+                      {item}
+                    </Select.Option>
+                  );
+                })}
+              </Select>
+            </div>
+            <div>
+              <span>Skill Level</span>
+              <Select
+                // mode="multiple"
+                showSearch
+                value={cookingSkill.length > 0 ? cookingSkill : undefined}
+                onChange={handleSkillSelection}
+                placeholder="Skill Level"
+                style={{ width: "100%" }}
+              >
+                {skills.map((item, index) => {
+                  return (
+                    <Select.Option key={index} value={item}>
+                      {item}
+                    </Select.Option>
+                  );
+                })}
+              </Select>
+            </div>
+            <div>
+              <span>Household Size</span>
+              <InputNumber min={1} max={10} defaultValue={1} />
+            </div>
+            <div>
+              <span>Gender</span>
+              <Select
+                // mode="multiple"
+                showSearch
+                placeholder="Gender"
+                value={gender ? gender : undefined}
+                onChange={handleGenderSelection}
+                style={{ width: "100%" }}
+              >
+                {["Male", "Female", "Other"].map((item, index) => {
+                  return (
+                    <Select.Option key={index} value={item}>
+                      {item}
+                    </Select.Option>
+                  );
+                })}
+              </Select>
+            </div>
+          </div>
+        </div>
+      </div>
+    </ProfileBody>
+  );
 };
 
 export default Profile;
