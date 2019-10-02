ca0d487a7b5f62e0bc0543f285ff451005656d85
Phil Arfuso <arfusop@gmail.com>
2019-10-02 17:24:19 -0400

    Added ui updates to recipe card.

--- src/components/layout/dashboard/components/RecipeCard.js
+++ src/components/layout/dashboard/components/RecipeCard.js
@@ -1,18 +1,24 @@
 import React from "react";
+import { Card, Icon, Rate } from "antd";
 
 const RecipeCard = () => {
   return (
-    <div>
-      Recipe image
-      <br />
-      Recipe name
-      <br />
-      Recipe rating
-      <br />
-      Recipe difficulty
-      <br />
-      Recipe author
-    </div>
+    <Card
+      hoverable
+      actions={[
+        <Icon type="setting" key="setting" />,
+        <Icon type="edit" key="edit" />,
+        <Icon type="delete" key="delete" />
+      ]}
+    >
+      <div classBame="cardLeft">
+        Title Category Author Date published/edited
+      </div>
+      <div classBame="cardRight">
+        Rating: <Rate character={<Icon type="star" />} allowHalf />
+        Difficulty: <Rate character={<Icon type="scissor" />} allowHalf />
+      </div>
+    </Card>
   );
 };
 
--- src/components/layout/styled/DashboardBody.js
+++ src/components/layout/styled/DashboardBody.js
@@ -70,8 +70,8 @@ const DashboardBody = styled.div`
   /* RECIPES */
   .recipesContainer {
     display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    grid-template-rows: repeat(4, 1fr);
+    grid-template-columns: repeat(3, 1fr);
+    /* grid-template-rows: repeat(4, 1fr); */
     grid-gap: 20px;
   }
 `;
