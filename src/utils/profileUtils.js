6f1d9d9d490d0c20195d25e4ddfe7e61989a4ad4
Your Name <arfusop@gmail.com>
2019-10-28 07:33:24 -0400

    Tried adding in logic to update DB with edited profile.

--- src/utils/profileUtils.js
+++ src/utils/profileUtils.js
@@ -1,7 +1,19 @@
+import Axios from "axios";
+
 // update the users profile in the DB
 export const updateUserProfile = async data => {
 	const url = 'https://maincourse-backend.herokuapp.com/api/profile';
   console.log(data);
   
   // get local storage cookie to set headers
+  const config = {
+    headers: {
+      "x-auth-token": localStorage.getItem('token')
+    }
+  }
+
+  const body = JSON.stringify(data);
+  const res = await Axios.post(url, body, config);
+
+  console.log(res);
 };
